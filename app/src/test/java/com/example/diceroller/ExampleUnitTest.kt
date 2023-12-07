d2b448ad0f8db4d9f009921c76559e0cce6dc2ad
Vlad <Vlad17.lutsk@gmail.com>
2023-12-07 18:30:39 +0200

    creating_test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -11,7 +11,8 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number() {
+        val dice = MainActivity.Dice(6)
+        val rollResult = dice.roll()
     }
 }
