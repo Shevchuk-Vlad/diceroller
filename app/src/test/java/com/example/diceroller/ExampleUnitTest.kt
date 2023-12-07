5d731c5e12dfed35e195035b38d157d92c5a5e00
Vlad <Vlad17.lutsk@gmail.com>
2023-12-07 18:31:50 +0200

    working_test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -14,5 +14,6 @@ class ExampleUnitTest {
     fun generates_number() {
         val dice = MainActivity.Dice(6)
         val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
